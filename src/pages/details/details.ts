59beb3f303fe612a08873cd4dac8a55c6c896739
Ulises <ulisesmc1006@gmail.com>
2018-05-09 16:39:04 -0500

    Examen diagnostico

---
+++ src/pages/details/details.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the DetailsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-details',
+  templateUrl: 'details.html',
+})
+export class DetailsPage {
+
+  item;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item = navParams.data.item;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetailsPage');
+  }
+
+  back(){
+    this.navCtrl.push(HomePage);
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams } from 'ionic-angular';
+import { DetailsPage } from '../details/details';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  personajes = [];
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.personajes= [
+      {
+        img:'assets/img/han-solo.jpg',
+        nombre: 'Han Solo',
+        titulo: 'Cazarrecompensas'
+      },
+      {
+        img:'assets/img/rey.jpg',
+        nombre: 'Rey',
+        titulo: 'Recolectora'
+      },
+      {
+        img:'assets/img/finn.jpg',
+        nombre: 'Finn',
+        titulo: 'Piloto'
+      },
+      {
+        img:'assets/img/leia.jpg',
+        nombre: 'Leia',
+        titulo: 'Princesa'
+      },
+    ];
+    
+  }
+
+  goToDetails(item){
+    this.navCtrl.push(DetailsPage, {item:item});
   }
 
 }
